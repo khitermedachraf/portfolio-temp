5a7d4d751ca5f82cf5f425a5d44227486c240b02
Mavericks Balitaan <balitaanmavericks@gmail.com>
2022-04-01 00:10:50 +0500

    Fix linter errors

--- script.js
+++ script.js
@@ -1,21 +1,21 @@
-const openBtn = document.querySelector('.openBtn')
+const openBtn = document.querySelector('.openBtn');
 const closeBtn = document.querySelector('.closeBtn');
 const modalContent = document.querySelector('.modalContent');
+
+function openModal() {
+  modalContent.style.display = 'block';
+}
+
+function closeModal() {
+  modalContent.style.display = 'none';
+}
+
+window.onclick = function global(event) {
+  if (event.target === modalContent) {
+    modalContent.style.display = 'none';
+  }
+};
 
 openBtn.addEventListener('click', openModal);
 closeBtn.addEventListener('click', closeModal);
 modalContent.addEventListener('click', closeModal);
-
-function openModal() {
-  modalContent.style.display = "block";
-}
-
-function closeModal() {
-  modalContent.style.display = "none";
-}
-
-window.onclick = function(event) {
-  if (event.target == modalContent) {
-    modal.style.display = "none";
-  }
-}
