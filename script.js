618f873c67aec684f93f6ed4013ff471d5800269
Mavericks Balitaan <balitaanmavericks@gmail.com>
2022-04-04 16:26:22 +0500

    Initial commit of popup branch

--- script.js
+++ script.js
@@ -19,3 +19,35 @@ window.onclick = function global(event) {
 openBtn.addEventListener('click', openModal);
 closeBtn.addEventListener('click', closeModal);
 modalContainer.addEventListener('click', closeModal);
+
+// Mobile Modal 1
+const seeProjectMobileModal1 = document.getElementById('seeProjectMobileModal1');
+const mobileModal = document.querySelector('.mobileModal');
+const closeMobile = document.querySelector('.closeMobile');
+const projectImgMobile = document.querySelector('.projectImgMobile');
+
+const projectObj = {
+  name: 'Heading 1',
+  image : ['./assets/projectImage00.png', './assets/projectImage01.png']
+}
+
+seeProjectMobileModal1.addEventListener('click', openMobileModal);
+closeMobile.addEventListener('click', closeMobileModal);
+mobileModal.addEventListener('click', closeMobileModal);
+
+function openMobileModal() {
+  console.log('It is working');
+  mobileModal.style.display = "block";
+  projectImgMobile.setAttribute('src', projectObj.image[1])
+  
+}
+
+function closeMobileModal() {
+  mobileModal.style.display = "none";
+}
+
+
+
+
+
+console.log(projectImgMobile);
