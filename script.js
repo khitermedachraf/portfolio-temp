42d4f83b88579c4eec600302104ab6c3d3d8b2a0
Mavericks Balitaan <balitaanmavericks@gmail.com>
2022-04-05 13:06:48 +0500

    Fix linter errors

--- script.js
+++ script.js
@@ -24,8 +24,7 @@ modalContainer.addEventListener('click', closeModal);
 const seeProject = document.querySelectorAll('.seeProject');
 const mobileModal = document.querySelectorAll('.mobileModal');
 
-const description0 =
-  "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea";
+const description0 = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea";
 
 const projectObj = {
   image: [
@@ -234,6 +233,7 @@ function openDesktopModal(e) {
   div3.appendChild(div2);
   desktopModal.appendChild(div1);
 }
+
 seeProjectDesktop.forEach((element) => {
   element.addEventListener('click', openDesktopModal);
 });
