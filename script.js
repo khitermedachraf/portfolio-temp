6e5d71de7556bdf4c9802d397a356df0d4189845
jodrack <drack.sir01@gmail.com>
2022-04-06 09:55:22 +0200

    Add email input form validation for deskto

--- script.js
+++ script.js
@@ -33,7 +33,8 @@ for (let i = 0; i < gridItem.length; i += 1) {
   gridItem[i].append(mobileModal[i]);
 }
 
-const description0 = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea";
+const description0 =
+  "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea";
 
 const projectObj = {
   image: [
@@ -274,7 +275,9 @@ function validation() {
   const str = emailInputMobile.value.trim();
   const regEx = /[A-Z]/g;
   if (regEx.test(str)) {
-    submitBtnMobile.setCustomValidity('Sorry, this form has not been submitted. The content of the email field has to be in lower case. \n Please check your inputs and reload the page');
+    submitBtnMobile.setCustomValidity(
+      'Sorry, this form has not been submitted. The content of the email field has to be in lower case. \n Please check your inputs and reload the page'
+    );
     submitBtnMobile.reportValidity();
     isFormValid = false;
   } else {
@@ -287,8 +290,42 @@ formMobile.addEventListener('submit', (e) => {
   e.preventDefault();
   validation();
   if (isFormValid) {
-    submitBtnMobile.setCustomValidity('The form has been submitted. \n Thank you for contacting me.');
+    submitBtnMobile.setCustomValidity(
+      'The form has been submitted. \n Thank you for contacting me.'
+    );
     submitBtnMobile.reportValidity();
     formMobile.submit();
+  }
+});
+
+// Form Validation - Desktop
+const formDesktop = document.querySelector('.formDesktop');
+const emailInputDesktop = document.querySelector('.emailInputDesktop');
+const submitBtnDesktop = document.querySelector('.submitBtnDesktop');
+
+function validation() {
+  const str = emailInputDesktop.value.trim();
+  const regEx = /[A-Z]/g;
+  if (regEx.test(str)) {
+    submitBtnDesktop.setCustomValidity(
+      'Sorry, this form has not been submitted. The content of the email field has to be in lower case. \n Please check your inputs and reload the page.'
+    );
+    submitBtnDesktop.reportValidity();
+    isFormValid = false;
+  } else {
+    isFormValid = true;
+  }
+  return isFormValid;
+}
+
+formDesktop.addEventListener('submit', (e) => {
+  e.preventDefault();
+  validation();
+  if (isFormValid) {
+    submitBtnDesktop.setCustomValidity(
+      'The form has been submitted. \n Thank you for contacting me.'
+    );
+    submitBtnDesktop.reportValidity();
+    formDesktop.submit();
   }
 });
